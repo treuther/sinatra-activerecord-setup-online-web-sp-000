38b4040a06737e9394f4706007f4dfb00f8730b9
Tracy Reuther <tracyreuther@gmail.com>
2019-09-14 00:13:33 +0000

    Automatically backed up by Learn

--- db/migrate/20190914000523_create_dogs.rb
+++ db/migrate/20190914000523_create_dogs.rb
@@ -1,7 +1,11 @@
-class CreateDogs < ActiveRecord::Migration[5.2]
-  def up
+class CreateDogs < ActiveRecord::Migration[5.2] #ActiveRecord version is REQUIRED. You MUST ALWAYS have this.
+  def up  # creates the table
+    create_table :dogs do |t|
+      t.string :name
+      t.string :breed
+    end
   end
 
-  def down
+  def down # drops the table
   end
 end
